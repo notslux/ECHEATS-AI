16948a565b73abf798f564c5ccb759578617c404
Slux <142702648+notslux@users.noreply.github.com>
2023-11-27 21:28:58 -0600

    Add files via upload

---
+++ ECHEATSWPF/Class/Bools.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AimmyWPF.Class
+{
+    class Bools
+    {
+        public static bool AIAimAligner = false;
+        public static bool AIPredictions = false;
+        public static bool ThirdPersonAim = false;
+        public static bool Triggerbot = false;
+        public static bool CollectDataWhilePlaying = false;
+        public static bool TopMost = false;
+    }
+}
